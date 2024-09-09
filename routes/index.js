a832a0b1f69494b718d61fa2cb9503bf1112b745
Netipong Sanklar <jj_sanklar@hotmail.co.th>
2024-09-09 21:56:48 +0700

    sub category + path is not yet finish

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,46 @@ router.get('/location', function(req, res, next) {
   res.render('location', { title: 'Location' });
 });
 
+router.get('/location', function(req, res, next) {
+  res.render('location', { title: 'Location' });
+});
+
+////category/////
+
+router.get('/category/dog', function(req, res, next) {
+  res.render('category/dog', { title: 'Location' });
+});
+
+router.get('/category/cat', function(req, res, next) {
+  res.render('category/cat', { title: 'Location' });
+});
+
+router.get('/category/fish', function(req, res, next) {
+  res.render('category/fish', { title: 'Location' });
+});
+
+router.get('/category/chicken', function(req, res, next) {
+  res.render('category/chicken', { title: 'Location' });
+});
+
+router.get('/category/pig', function(req, res, next) {
+  res.render('category/pig', { title: 'Location' });
+});
+
+router.get('/category/cow', function(req, res, next) {
+  res.render('category/cow', { title: 'Location' });
+});
+
+router.get('/category/bird', function(req, res, next) {
+  res.render('category/bird', { title: 'Location' });
+});
+
+router.get('/category/accessory', function(req, res, next) {
+  res.render('category/dog', { title: 'Location' });
+});
+
+router.get('/category/medicine', function(req, res, next) {
+  res.render('category/medicine', { title: 'Location' });
+});
+
 module.exports = router;
